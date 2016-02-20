1337ef18f7a85ccb9cfa7f6d201982a67766a1cf
HDijab <halil.dijab@icloud.com>
2016-02-20 16:09:34 +0100

    Initial Authentication

--- HydeParc/db/seeds.rb
+++ HydeParc/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |variable|
+  User.create(
+    nickname: Faker::Internet.user_name,
+    email: Faker::Internet.safe_email,
+    password: 'testpassword',
+    password_confirmation: 'testpassword'
+  )
+end
