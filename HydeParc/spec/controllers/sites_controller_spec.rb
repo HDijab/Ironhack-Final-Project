8346ddb2b5e5990307168a3f1ccac427b06aeede
HDijab <halil.dijab@icloud.com>
2016-02-22 11:55:08 +0100

    Sign Up Styling

--- HydeParc/spec/controllers/sites_controller_spec.rb
+++ HydeParc/spec/controllers/sites_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe SitesController, type: :controller do
+  describe '#home' do
+    it 'renders the home template' do
+      get(:home)
+      expect(response).to render_template('home')
+    end
+  end
 
+  describe '#easy_walk' do
+    it 'renders the easy_walk template' do
+      get(:easy_walk)
+      expect(response).to render_template('easy_walk')
+    end
+  end
 end
--- HydeParc/spec/controllers/users_controller_spec.rb
+++ HydeParc/spec/controllers/users_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+  it "does nothing because there isn't anything in the damn file yet"
 end
