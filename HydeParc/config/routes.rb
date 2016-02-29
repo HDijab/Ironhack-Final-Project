01c0a49c71a8e69521983dfaf45f6886985abb06
HDijab <halil.dijab@icloud.com>
2016-02-29 12:33:12 +0100

    Created blank views

--- HydeParc/config/routes.rb
+++ HydeParc/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :users, except: [:new, :create, :index]
 
   resources :communities do
     resources :posts, except: :index do
